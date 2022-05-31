eff19ad39416cb0e853cf8ffb1f5ebe0d869123a
Alexandr Drankevich <a.a.drankevich@gmail.com>
2022-06-01 01:13:10 +0300

    refactor

--- src/test/java/com/tvmaze/ui/utils/DateConverter.java
+++ src/test/java/com/tvmaze/ui/utils/DateConverter.java
@@ -1,21 +1,9 @@
 package com.tvmaze.ui.utils;
 
+import java.time.Month;
+
 public class DateConverter {
     public static int monthToInt(String month){
-        switch (month){
-            case "January": return 1;
-            case "February ": return 2;
-            case "March": return 3;
-            case "April": return 4;
-            case "May": return 5;
-            case "June": return 6;
-            case "July": return 7;
-            case "August": return 8;
-            case "September": return 9;
-            case "October": return 10;
-            case "November": return 11;
-            case "December": return 12;
-        }
-        return 0;
+          return Month.valueOf(month.toUpperCase()).getValue();
     }
 }
